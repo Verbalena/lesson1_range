ce2d8126d6761f27415d353985bf060f5fe1584f
Verbalena <alen.verb@yandex.ru>
2024-09-17 00:35:33 +0300

    Update lesson_1_range.py
    Убрала print() и нумерацию задач

--- lesson_1_range.py
+++ lesson_1_range.py
@@ -5,16 +5,12 @@
 list_1 = ['яблоко', 'груша', 'виноград', 'абрикос', 'вишня']
 list_2 = ['муха', 'комар', 'стрекоза', 'богомол', 'ваза']
 # 1.1
-print('Задача 1. Вариант 1')
-
 length_lines = (len(x) * len(y) for x, y in zip(list_1, list_2) if len(x) != len(y))
 print(list(length_lines))
 result = [x[0] == y[0] for x, y in zip(list_1, list_2) if len(x) != len(y)]
 print(result)
-print()
-# 1.2
-print('Задача 1. Вариант 2')
 
+# 1.2
 for x, y in zip(list_1, list_2):
     if len(x) != len(y):
         res = len(x) * len(y)
@@ -27,15 +23,13 @@ for x, y in zip(list_1, list_2):
 # русском. Преобразуйте слова в верхний регистр, если английская версия длиннее.
 # Проверьте, совпадают ли строки с русской и английской стороны при чтении с конца. """
 
-print()
-print('Задача 2')
 animal_rus = ['медведь', 'заяц', 'волк', 'мышь', 'гепард']
 animal_eng = ['bear', 'hare', 'wolf', 'mouse', 'cheetah']
 
 for x,y in zip(animal_rus, animal_eng):
     if len(x) < len(y):
         print(x.upper(), y.upper())
-print()  # пустая строка
+
 # # Проверьте, совпадают ли строки с русской и английской стороны при чтении с конца.
 animal_rus_reverse = animal_rus[::-1]
 print(animal_rus_reverse)
@@ -52,8 +46,7 @@ for x,y in zip(animal_eng_reverse, animal_rus):
 # # """3. Задача 3: Создайте списки с названиями цветов на английском и русском.
 # # Объедините строки в парах, где русская версия длиннее.
 # # Проверяйте, длиннее ли английские слова или равны русским."""
-print()
-print('Задача 3')
+
 flowers_rus = ['фиалка', 'ромашка', 'роза', 'тюльпан', 'ирис']
 flowers_eng = ['violet', 'chamomile', 'rose', 'tulip', 'Iris']
 # 1 вариант. В списке (result) представлены кортежи из пар, где русское слово длиннее английского.
@@ -79,8 +72,7 @@ for x in range(len(flowers_rus)):
 #  # 4. Задача 4: Задайте списки светил на английском и русском. Возьмите первые
 #  #три буквы английских слов в парах, где длина русских слов в два раза
 #  #больше. Проверьте, заканчиваются ли русские слова на 'а'."""
-print()
-print('Задача 4')
+
 planets_eng = ['Aldebaran', 'Sirius', 'Elektra', 'Altair', 'Aliot']
 planets_rus = ['Альдебаран', 'Сириус', 'Электра', 'Альтаир', 'Алиот']
 
